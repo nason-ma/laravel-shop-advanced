3ad9b828fc4ad6734b136ec0aba7b04a6995463e
Nason <878826676@qq.com>
2021-03-23 21:08:10 +0800

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -42,9 +42,17 @@ class Product extends BaseModel
 {
     use HasFactory;
 
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'rating', 'sold_count', 'review_count', 'price',
+        'category_id', 'type'
     ];
 
     protected $casts = [
@@ -69,5 +77,10 @@ class Product extends BaseModel
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
