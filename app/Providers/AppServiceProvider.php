a2e9b71a532150931d1ea858d7501a91ac77749c
Nason <878826676@qq.com>
2021-03-30 18:59:50 +0800

    引入 Elasticsearch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Monolog\Logger;
 use Yansongda\Pay\Pay;
+use Elasticsearch\ClientBuilder as ESClientBuilder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -49,6 +50,19 @@ class AppServiceProvider extends ServiceProvider
 
             // 调用 Yansongda\Pay 来创建一个微信支付对象
             return Pay::wechat($config);
+        });
+
+        // 注册一个名为 es 的单例
+        $this->app->singleton('es', function () {
+            // 从配置文件读取 Elasticsearch 服务器列表
+            $hosts = config('database.elasticsearch.hosts');
+            $builder = ESClientBuilder::create()->setHosts($hosts);
+            // 如果是开发环境
+            if (app()->environment() === 'local') {
+                // 配置日志，Elasticsearch 的请求和返回数据将打印到日志文件中，方便调试
+                $builder->setLogger(app('log')->getLogger());
+            }
+            return $builder->build();
         });
     }
 
